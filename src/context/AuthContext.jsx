80768e3b8dd7a9230571bdaf3de8e5fca15bc5f9
shubham maurya <sm7758525@gmail.com>
2026-08-16 01:50:52 +0530

    use VITE_BACKEND_URL in AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
-    let serverUrl = "http://localhost:8000"
+    let serverUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000"
 
     let value = {
        serverUrl
