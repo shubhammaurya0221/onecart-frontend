046cf57ab11c1cc02e079673c76a4de6afe5f3f6
shubham maurya <sm7758525@gmail.com>
2026-08-16 16:32:15 +0530

    auth error

--- utils/Firebase.js
+++ utils/Firebase.js
@@ -2,12 +2,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "loginonecart-ef5bf.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "loginonecart-ef5bf",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "loginonecart-ef5bf.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "105446562430",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:105446562430:web:cdb0565d77d562f448962f"
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY || "AIzaSyCsywbj51ooNtOUTxWcs9zYNNSDKuPPbdA",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "onecart-8bf8d.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "onecart-8bf8d",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "onecart-8bf8d.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "183076968970",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:183076968970:web:7290298f41dd99f0e2ddf3"
 };
 
 const app = initializeApp(firebaseConfig);
